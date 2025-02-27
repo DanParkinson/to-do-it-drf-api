7e22cf45d43454668c7fa77f678c2c143f98681a
DanParkinson <danny.parkinson1996@gmail.com>
2025-02-27 18:00:29 +0000

    Added profile url for delete view

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,4 +4,5 @@ from profiles import views
 urlpatterns = [
     path('profiles/', views.ProfileListView.as_view()),
     path('profiles/<int:pk>/', views.ProfileDetailView.as_view()),
+    path("delete-account/", views.DeleteAccountView.as_view()),
 ]
